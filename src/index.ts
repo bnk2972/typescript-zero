4f9281a80963bb7ff22555d4f8ce7128dff0449e
BNK 2972 <beer@livingmobile.me>
2021-09-25 23:49:14 +0700

    Overloading Function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-// function getFullName(firstName: string, lastName: string) {
-//     return `${firstName} ${lastName}`
-// }
+//Overloading เป็นฟีเจอร์ที่สามารถอนุญาตให้ตั้งชื่อฟังก์ชันซ้ำกันได้
+//แต่ parameter & return type คือต้องแตกต่างกัน
+function addInt(n: number, m: number): number
+function addInt(n: string, m: string): string
+function addInt(n: unknown, m: unknown): unknown {
+    if (typeof n === 'number' && typeof m === 'number') return n + m
+    if (typeof n === 'string' && typeof m === 'string') {
+        return `${parseInt(n) + parseInt(m)}`
+    }
 
-// const getFullName = function(firstName: string, lastName: string): string {
-//     return `${firstName} ${lastName}`
-// }
-
-type getFullNameFn = (firstName: string, lastName: string) => string
-
-const getFullName: getFullNameFn = (firstName, lastName) => {
-    return `${firstName} ${lastName}`
+    throw new Error('must be the same type')
 }
+
+console.log(addInt(1, 2))
+console.log(addInt('3', '4'))
+// console.log(addInt(1, '3'))
