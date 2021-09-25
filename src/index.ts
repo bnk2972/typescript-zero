4d6e777674871b84fd1d36a42c970e34dbb1b132
BNK 2972 <beer@livingmobile.me>
2021-09-25 23:39:24 +0700

    Function

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,13 @@
-interface Person {
-    name: string;
-    age: number;
-    gender: string;
-}
-
-//Excess Property Checks #1
-// const person = {
-//     name: 'Beer',
-//     age: 28,
-//     gender: 'Male',
-//     job: 'Developer'
+// function getFullName(firstName: string, lastName: string) {
+//     return `${firstName} ${lastName}`
 // }
 
-// const beer: Person = person
-
-// const person: Person = {
-//     name: 'Beer',
-//     age: 28,
-//     gender: 'Male',
-//     job: 'Developer'
+// const getFullName = function(firstName: string, lastName: string): string {
+//     return `${firstName} ${lastName}`
 // }
 
-interface Regtangle {
-    width: number;
-    height: number;
+type getFullNameFn = (firstName: string, lastName: string) => string
+
+const getFullName: getFullNameFn = (firstName, lastName) => {
+    return `${firstName} ${lastName}`
 }
-
-function getArea(shape: Regtangle) {
-    return shape.width * shape.height
-}
-
-//Excess Property Checks #2
-// const shape = { width: 10, height: 20, depth: 10 }
-// getArea(shape)
-
-// getArea({ width: 10, height: 20, depth: 10 })
-
-// const shape: Regtangle = { width: 10, height: 20, depth: 10 }
-// getArea(shape)
