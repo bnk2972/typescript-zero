49980672605a4ca9f55ad8acff1838ce5a4b5caf
BNK 2972 <beer@livingmobile.me>
2021-09-25 20:57:59 +0700

    Array Type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-let num: unknown
-num = 100
-let numFixed = (num as number).toFixed(2)
+let num1: Array<number> = [1, 2, 3]
+let arr1: Array<unknown> = [1, '2', 'beer']
+
+let num2: number[] = [1, 2, 3]
+let arr2: unknown[] = [1, 2, false, 'beer']
