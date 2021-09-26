518998fe0bb80a113076acae2d7992217173cf96
BNK 2972 <beer@livingmobile.me>
2021-09-26 16:45:21 +0700

    Classes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
-//Key Remapping
-type Person = {
-    name: string;
-    age: number;
-}
-
-// type PersonSubscribers = {
-//     nameChanged: (key: "name", value: string) => void;
-//     ageChanged: (key: "age", value: number) => void;
+// const beer = {
+//     name: 'Nattapadtanasak',
+//     age: 28,
+//     printDetail() {
+//         console.log('name: ', this.name, 'age: ', this.age)
+//     }
 // }
 
-type PersonSubscribers = {
-    [K in keyof Person as `${K}Changed`]: (key: K, value: Person[K]) => void;
+// const maree = {
+//     name: 'Maree',
+//     age: 25,
+//     printDetail() {
+//         console.log('name: ', this.name, 'age: ', this.age)
+//     }
+// }
+
+class Person {
+    static University = 'Kasetsart'
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) { // ผู้สร้าง
+        this.name = name
+        this.age = age
+    }
+
+    printDetail() {
+        console.log('name: ', this.name, 'age: ', this.age, Person.University)
+    }
 }
+
+const beer = new Person('Beer', 28)
+console.log(beer)
+const malee = new Person('Melee', 25)
+console.log(malee)
+malee.printDetail()
