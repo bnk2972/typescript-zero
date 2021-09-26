bed37ca0271272949c7946e8b52ba282c1f1c19d
BNK 2972 <beer@livingmobile.me>
2021-09-26 16:14:06 +0700

    Indexable Types

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-//Keyof
-interface Person {
-    name: string;
-    age: number;
-    address: string;
+//Indexable Types
+interface States {
+    [ state: string ]: boolean;
+    // writeable: number;
 }
 
-// type PersonKeys = 'name' | 'age' | 'address'
-type PersonKeys = keyof Person
-// type PersonAgeType = number
-type PersonAgeType = Person['age']
-// type PersonValue = string | number
-// type PersonValue = Person['name' | 'age' | 'address']
-type PersonValue = Person[keyof Person]
-
-const person: Person = {
-    name: 'beer',
-    age: 28,
-    address: 'NK'
-}
-
-function printValuePerson(key: keyof Person): void {
-    console.log(person[key])
-}
-
-printValuePerson('name')
+const states: States = { enabled: true, readable: true, /* writeable: 123 */ }
