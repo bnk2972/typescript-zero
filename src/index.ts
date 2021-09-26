80e9d45b22cd497b6072f020943d4395880814fc
BNK 2972 <beer@livingmobile.me>
2021-09-26 21:25:42 +0700

    ReturnType

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-type MyRecord<T extends string | number | symbol, U> = {
-    [K in T]: U;
+type MyReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+
+type Person = {
+    name: string;
+    age: number;
 }
 
-// type MyPerson = {
-//  name: string;
-//  address: string;
-// }
-// type MyPerson = MyRecord<'name' | 'address', string>
-type Person = Record<'name' | 'address', string>
+function get(person: Person, key: keyof Person) {
+    return person[key]
+}
+
+type Fn = typeof get
+
+// type myGetReturnType = MyReturnType<Fn>
+type myGetReturnType = ReturnType<Fn>
